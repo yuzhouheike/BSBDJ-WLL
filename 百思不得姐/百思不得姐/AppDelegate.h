acf0ec68ebd2e48e6c5c487dd8cc3fd2dd7e7c84
c ma <mac@hades.local>
2016-03-21 20:47:27 +0800

    first

---
+++ 百思不得姐/百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by ma c on 16/3/21.
+//  Copyright © 2016年 王磊磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 百思不得姐/百思不得姐/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  百思不得姐
+//
+//  Created by ma c on 16/3/21.
+//  Copyright © 2016年 王磊磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UITabBarController
+
+@end
